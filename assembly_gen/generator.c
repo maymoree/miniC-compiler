86cae0b6b22b81ca57bbfcbbecd1b905835bdbef
Memo Tangtipongkul <memo.tangtipongkul.26@dartmouth.edu>
2024-05-30 03:25:25 -0400

    register allocation seems to be working i think

--- assembly_gen/generator.c
+++ assembly_gen/generator.c
@@ -7,19 +7,21 @@
 #include <set>
 #include <string>
 #include <iostream>
+#include <algorithm>
+
 
 using namespace std;
 
-map<LLVMValueRef, int> register_allocation(LLVMModuleRef mod);
+map<LLVMValueRef, int> register_allocation(LLVMModuleRef mod, map<LLVMValueRef, int> &inst_index, map<LLVMValueRef, pair<int, int>> &live_range);
 void compute_liveness(LLVMBasicBlockRef basicBlock, map<LLVMValueRef, int> &inst_index, map<LLVMValueRef, pair<int, int>> &live_range);
-// LLVMValueRef find_spill(LLVMValueRef instruc, map<LLVMValueRef, int> &reg_map, map<LLVMValueRef, int> &inst_index,
-//                         vector<LLVMValueRef> &sorted_list, map<LLVMValueRef, pair<int, int>> &live_range);
+LLVMValueRef find_spill(LLVMValueRef instruc, map<LLVMValueRef, int> &reg_map, map<LLVMValueRef, int> &inst_index,
+                        vector<LLVMValueRef> &sorted_list, map<LLVMValueRef, pair<int, int>> &live_range);
 // map<LLVMBasicBlockRef, char*> createBBLabels(LLVMValueRef func);
 // void printDirectives(LLVMModuleRef module, LLVMValueRef function, FILE* fp); // not sure yet
 // void printFunctionEnd(FILE* fp); // not sure...
 // map<LLVMValueRef, int> getOffsetMap(LLVMValueRef func, int* local_mem); // ???
 void generator(LLVMModuleRef mod); // ???
-// vector<LLVMValueRef> sortList(vector<LLVMValueRef> &sorted_list); // have to sort list somehow
+vector<LLVMValueRef> sort_list(map<LLVMValueRef, pair<int, int>> &live_range);
 
 map<int, string> REGISTER_MAP = {
     {0, "ebx"},
@@ -28,10 +30,10 @@ map<int, string> REGISTER_MAP = {
     {-1, "spill"}
 };
 
-map<LLVMValueRef, int> register_allocation(LLVMModuleRef mod) {
+map<LLVMValueRef, int> register_allocation(LLVMModuleRef mod, map<LLVMValueRef, int> &inst_index, map<LLVMValueRef, pair<int, int>> &live_range) {
     // only for those instructions that generate value (spilled = -1)
 
-    map<LLVMValueRef, int> map_return;
+    map<LLVMValueRef, int> reg_map;
     // loop through functions
 	for (LLVMValueRef function = LLVMGetFirstFunction(mod); 
 	function; 
@@ -46,32 +48,122 @@ map<LLVMValueRef, int> register_allocation(LLVMModuleRef mod) {
 
             printf("\nin block\n");
 
-            set<int> registers {0, 1, 2}; // initialize registers
-
-            // populates the maps that already exist
-            map<LLVMValueRef, int> inst_index;
-            map<LLVMValueRef, pair<int, int>> live_range;
+            set<int> registers {0, 1, 2}; // initialize available registers
 
             compute_liveness(basicBlock, inst_index, live_range);
 
+            for (LLVMValueRef instruction = LLVMGetFirstInstruction(basicBlock); 
+            instruction; 
+            instruction = LLVMGetNextInstruction(instruction)) {
 
-            // for (LLVMValueRef instruction = LLVMGetFirstInstruction(basicBlock); 
-            // instruction; 
-            // instruction = LLVMGetNextInstruction(instruction)) {
+                LLVMOpcode instruc_opcode = LLVMGetInstructionOpcode(instruction);
 
-            // }
+                // alloc instructions
+                if (instruc_opcode == LLVMAlloca) { // check if it's an alloc instruction
+                    continue;
+                }
+
+                if (instruc_opcode == LLVMStore || instruc_opcode == LLVMBr || instruc_opcode == LLVMCall) { // no result instructions
+                    
+                    // loop through instruc's operands to check if any of it has ended
+                    int num_operands = LLVMGetNumOperands(instruction);
+                    for (int i = 0; i < num_operands; i++) {
+                        LLVMValueRef op = LLVMGetOperand(instruction, i);
+                        if (live_range.count(op) && reg_map.count(op)) { // if operand exists in live_range and reg_map
+                            // if the instruction ends, and it is assigned to a register (not spill)
+                            if (live_range.at(op).second == inst_index.at(instruction) && reg_map.at(op) != -1) {
+                                registers.insert(reg_map.at(op));
+                            }
+                        }
+                    }
+
+                }
+                else { 
+                    if (instruc_opcode == LLVMAdd || instruc_opcode == LLVMSub || instruc_opcode == LLVMMul) { // if add sub or mul
+
+                        LLVMValueRef first_op = LLVMGetOperand(instruction, 0);
+
+                        // check if first op has physical register and it's liveness range ends at instruction
+                        if (reg_map.count(first_op) && reg_map.at(first_op) != -1 && live_range.at(first_op).second == inst_index.at(instruction)) {
+                            
+                            // add free register to reg_map with instruction
+                            int reg = reg_map.at(first_op);
+                            registers.erase(reg); // might be redundant
+                            reg_map.insert(pair<LLVMValueRef, int> (instruction, reg));
+
+                            // add free register from second operand to available registers
+                            LLVMValueRef second_op = LLVMGetOperand(instruction, 1);
+                            if (reg_map.count(second_op) && reg_map.at(second_op) != -1 && live_range.at(second_op).second == inst_index.at(instruction)) {
+                                registers.insert(reg_map.at(second_op));
+                            }
+
+                        }
+
+                    }
+                    else if (!registers.empty()) { // if there are registers available
+
+                        // get a register and add it to reg_map with current instruction
+                        auto first_reg = registers.begin(); 
+                        int reg = *first_reg;
+                        reg_map.insert(pair<LLVMValueRef, int> (instruction, reg));
+                        registers.erase(reg); 
+
+                        // loop through instruc's operands to check if any of it has ended
+                        int num_operands = LLVMGetNumOperands(instruction);
+                        for (int i = 0; i < num_operands; i++) {
+                            LLVMValueRef op = LLVMGetOperand(instruction, i);
+                            if (live_range.count(op) && reg_map.count(op)) { // if operand exists in live_range and reg_map
+                                // if the instruction ends, and it is assigned to a register (not spill)
+                                if (live_range.at(op).second == inst_index.at(instruction) && reg_map.at(op) != -1) {
+                                    registers.insert(reg_map.at(op));
+                                }
+                            }
+                        }
+
+                    }
+                    else {
+                        vector<LLVMValueRef> sorted_list = sort_list(live_range);
+                        LLVMValueRef spill = find_spill(instruction, reg_map, inst_index, sorted_list, live_range);
+
+                        // use liveness range to compare; if liveness range of instruc ends after that of V
+                        if (live_range.at(spill).second < live_range.at(instruction).second) {
+                            reg_map.insert(pair<LLVMValueRef, int> (instruction, -1));
+                        } else {
+                            int reg = reg_map.at(spill);
+                            reg_map.insert(pair<LLVMValueRef, int> (instruction, reg));
+                            
+                            reg_map.erase(spill);
+                            reg_map.insert(pair<LLVMValueRef, int> (spill, -1));
+                        }
+
+                        // loop through instruc's operands to check if any of it has ended
+                        int num_operands = LLVMGetNumOperands(instruction);
+                        for (int i = 0; i < num_operands; i++) {
+                            LLVMValueRef op = LLVMGetOperand(instruction, i);
+                            if (live_range.count(op) && reg_map.count(op)) { // if operand exists in live_range and reg_map
+                                // if the instruction ends, and it is assigned to a register (not spill)
+                                if (live_range.at(op).second == inst_index.at(instruction) && reg_map.at(op) != -1) {
+                                    registers.insert(reg_map.at(op));
+                                }
+                            }
+                        }
+                            
+                    }
+                
+                }
+                
+            }
         }
 
     }
 
-    return map_return;
+    return reg_map;
 }
 
 void compute_liveness(LLVMBasicBlockRef basicBlock, map<LLVMValueRef, int> &inst_index, map<LLVMValueRef, pair<int, int>> &live_range) {
     // local liveness
 
     printf("\ncompute!!!\n");
-
 
     // first loop assigning indices; ignoring allocs
     int idx = 0;
@@ -122,28 +214,77 @@ void compute_liveness(LLVMBasicBlockRef basicBlock, map<LLVMValueRef, int> &inst
     }
 
     // check if liveness is correct
-    // Print inst_index for debugging
-    printf("\ninst index\n");
-    for (auto& entry : inst_index) {
-        LLVMValueRef instr = entry.first;
-        int index = entry.second;
-        LLVMDumpValue(instr);
-        printf(" --> index: %d\n", index);
+    // printf("\ninst index\n");
+    // for (auto& entry : inst_index) {
+    //     LLVMValueRef instr = entry.first;
+    //     int index = entry.second;
+    //     LLVMDumpValue(instr);
+    //     printf(" --> index: %d\n", index);
+    // }
+    // printf("\nlive ranges\n");
+    // for (const auto& entry : live_range) {
+    //     LLVMValueRef instr = entry.first;
+    //     pair<int, int> range = entry.second;
+    //     LLVMDumpValue(instr);
+    //     printf(" --> range: %d - %d \n", range.first, range.second);
+    // }
+
+}
+
+// sort list by the endpoints in live_map (decreasing order)
+vector<LLVMValueRef> sort_list(map<LLVMValueRef, pair<int, int>> &live_range) {
+    vector<LLVMValueRef> sorted;
+
+    // Copy keys from live_range to sorted vector
+    for (const auto &instruc : live_range) {
+        sorted.push_back(instruc.first);
     }
 
-    // Print live_range for debugging
-    printf("\nlive ranges\n");
-    for (const auto& entry : live_range) {
-        LLVMValueRef instr = entry.first;
-        pair<int, int> range = entry.second;
-        LLVMDumpValue(instr);
-        printf(" --> range: %d - %d \n", range.first, range.second);
+    // Custom comparator to sort by the endpoint in live_range in decreasing order
+    sort(sorted.begin(), sorted.end(), [&live_range](LLVMValueRef a, LLVMValueRef b) {
+        return live_range[a].second > live_range[b].second;
+    });
+
+    return sorted;
+}
+
+LLVMValueRef find_spill(LLVMValueRef instruc, map<LLVMValueRef, int> &reg_map, map<LLVMValueRef, int> &inst_index, vector<LLVMValueRef> &sorted_list, map<LLVMValueRef, pair<int, int>> &live_range) {
+
+    pair<int, int> instruc_live_range = live_range.at(instruc);
+
+    for (LLVMValueRef V : sorted_list) {
+
+        pair<int, int> V_live_range = live_range.at(V);
+
+        // check if V_live_range overlaps with instruc_live_range
+        // 1. V ends after instruc starts
+        // 2. V starts before instruc ends
+        bool overlap = (V_live_range.second > instruc_live_range.first || V_live_range.first < instruc_live_range.second);
+        if (overlap) {
+            // check if V is in reg_map and not -1 (not spilled)
+            if (reg_map.count(V) && reg_map.at(V) != -1) {
+                return V;
+            }
+        }
     }
 
+    return NULL;
 }
 
 void generator(LLVMModuleRef mod) {
 
-    map<LLVMValueRef, int> map_1 = register_allocation(mod);
+    // populates the maps that already exist --> have to pass them in as parameters
+    map<LLVMValueRef, int> inst_index;
+    map<LLVMValueRef, pair<int, int>> live_range;
+
+    map<LLVMValueRef, int> map_1 = register_allocation(mod, inst_index, live_range);
+
+    printf("\nreg_map!!!\n");
+    for (const auto& itr : map_1) {
+        LLVMValueRef instr = itr.first;
+        int reg = itr.second;
+        LLVMDumpValue(instr);
+        printf(" --> reg: %d \n", reg);
+    }
 
 }
